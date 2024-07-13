5c088eccb8968464c2467f80bc1dc2386a8667c3
joganis <joganis@hotmail.com>
2024-07-12 19:13:13 -0500

    primer commit : se organiza la estructura del proyecto

---
+++ src/main/java/com/petShop/PetShopApiPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.petShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetShopApiPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetShopApiPruebaApplication.class, args);
+	}
+
+}
